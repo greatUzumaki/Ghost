29824088b1621732189824f8ada35be40291e785
Steve Larson <9larsons@gmail.com>
2023-10-02 10:34:56 -0500

    Fix some tests...

--- ghost/admin/tests/acceptance/editor/lexical-test.js
+++ ghost/admin/tests/acceptance/editor/lexical-test.js
@@ -1,9 +1,7 @@
 import loginAsRole from '../../helpers/login-as-role';
 import {BLANK_DOC} from 'koenig-editor/components/koenig-editor';
 import {currentURL} from '@ember/test-helpers';
-import {enableLabsFlag} from '../../helpers/labs-flag';
 import {expect} from 'chai';
-import {find} from '@ember/test-helpers';
 import {setupApplicationTest} from 'ember-mocha';
 import {setupMirage} from 'ember-cli-mirage/test-support';
 import {visit} from '../../helpers/visit';
@@ -20,8 +18,6 @@ describe('Acceptance: Lexical editor', function () {
         config.attrs.editor = {url: 'https://cdn.pkg/editor.js'};
         config.save();
 
-        enableLabsFlag(this.server, 'lexicalEditor');
-
         // stub loaded external module to avoid loading of external dep
         window['@tryghost/koenig-lexical'] = {
             KoenigComposer: () => null,
@@ -30,7 +26,7 @@ describe('Acceptance: Lexical editor', function () {
     });
 
     it('redirects to signin when not authenticated', async function () {
-        await visit('/editor-beta/post/');
+        await visit('/editor/post/');
         expect(currentURL(), 'currentURL').to.equal('/signin');
     });
 
@@ -40,23 +36,15 @@ describe('Acceptance: Lexical editor', function () {
         config.save();
 
         await loginAsRole('Administrator', this.server);
-        await visit('/editor-beta/post/');
+        await visit('/editor/post/', 500);
 
         expect(currentURL(), 'currentURL').to.equal('/posts');
     });
 
     it('loads when editor.url is present', async function () {
         await loginAsRole('Administrator', this.server);
-        await visit('/editor-beta/post/');
-        expect(currentURL(), 'currentURL').to.equal('/editor-beta/post/');
-    });
-
-    it('shows feedback link in lexical editor', async function () {
-        await loginAsRole('Administrator', this.server);
-        await visit('/editor-beta/post/');
-        expect(currentURL(), 'currentURL').to.equal('/editor-beta/post/');
-
-        expect(find('.gh-editor-feedback'), 'feedback button').to.exist;
+        await visit('/editor/post/');
+        expect(currentURL(), 'currentURL').to.equal('/editor/post/');
     });
 
     it('redirects mobiledoc editor to lexical editor when post.lexical is present', async function () {
@@ -67,7 +55,7 @@ describe('Acceptance: Lexical editor', function () {
         await loginAsRole('Administrator', this.server);
         await visit(`/editor/post/${post.id}`);
 
-        expect(currentURL()).to.equal(`/editor-beta/post/${post.id}`);
+        expect(currentURL()).to.equal(`/editor/post/${post.id}`);
     });
 
     it('does not redirect to mobiledoc editor when post.mobiledoc is present', async function () {
@@ -76,8 +64,8 @@ describe('Acceptance: Lexical editor', function () {
         });
 
         await loginAsRole('Administrator', this.server);
-        await visit(`/editor-beta/post/${post.id}`);
+        await visit(`/editor/post/${post.id}`);
 
-        expect(currentURL()).to.equal(`/editor-beta/post/${post.id}`);
+        expect(currentURL()).to.equal(`/editor/post/${post.id}`);
     });
 });
